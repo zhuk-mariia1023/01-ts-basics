395f46c7f2a0d6583280c20d14db5557d4c51602
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-05-10 18:12:36 +0300

    Initial commit

---
+++ src/task-5.ts
@@ -0,0 +1,11 @@
+function logStatus(status: "loading" | "success" | "error"): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+logStatus("loading");
---
+++ src/task-6.ts
@@ -0,0 +1,7 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]); // 1
+getFirstElement<string>(["a", "b", "c"]); // "a"
+getFirstElement<boolean>([true, false, true]); // true
