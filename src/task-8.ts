dcb747651b40f38f641d85eb03f467d73be38c35
Mariia Zhuk <mariia.zhuk1023@gmail.com>
2025-05-10 19:12:29 +0300

    index.html corection

--- src/task-8.ts
+++ src/task-8.ts
@@ -8,7 +8,7 @@ interface Post {
 
 async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get<Post[]>(
-    "<https://jsonplaceholder.typicode.com/posts>"
+    "https://jsonplaceholder.typicode.com/posts"
   );
   return response.data;
 }
